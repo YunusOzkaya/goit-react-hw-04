ca8416ae017cbeac9122c637390af179c3355b43
YunusOzkaya <nasuhan.yunus.ozkaya@gmail.com>
2025-08-06 13:41:59 +0300

    upğdate loader.

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Circles } from "react-loader-spinner";
 import styles from "./Loader.module.css";
 
 const Loader = () => (
   <div className={styles.loader}>
-    <Circles color="#24292f" height={64} width={64} />
+    <div className={styles.spinner}></div>
   </div>
 );
 
