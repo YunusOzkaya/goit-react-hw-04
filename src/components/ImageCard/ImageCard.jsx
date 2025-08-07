58d96141e1f9c028322a0d5fd8ef03df7a68cd8e
Yunus Özkaya <92160164+YunusOzkaya@users.noreply.github.com>
2025-08-07 11:38:53 +0300

    Update ImageCard.jsx

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -3,11 +3,12 @@ import styles from "./ImageCard.module.css";
 
 const ImageCard = ({ image, onClick }) => {
   return (
-    <div className={styles.card} onClick={() => onClick(image)}>
+    <div className={styles.card}>
       <img
         src={image.urls.small}
         alt={image.alt_description || "Unsplash Image"}
         className={styles.img}
+        onClick={() => onClick(image)}
       />
     </div>
   );
